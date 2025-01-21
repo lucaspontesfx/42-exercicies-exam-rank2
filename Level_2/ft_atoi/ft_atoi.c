15cd26b3ab2b866420523637a6c15cdb4278092a
Lucas Pontes <lucaspontesfx>
2025-01-21 17:31:22 -0300

    atoi

--- Level_2/ft_atoi/ft_atoi.c
+++ Level_2/ft_atoi/ft_atoi.c
@@ -10,9 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_atoi(const char *str)
 {
 	int	res;
 	int	flag;
 	int	i;
+
+	res = 0;
+	flag = 1;
+	i = 0;
+	while ( str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			flag = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * flag);
 }
+/*
+int	main(int ac, char **av)
+{
+	int	res;
+
+	res = ft_atoi(av[1]);
+	printf("%d\n", res);
+	return (0);
+}*/
